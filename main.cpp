f13eb00158468d46e87ce1882f628edf2c6f34ea
xinchen <2019651597@qq.com>
2024-05-21 01:28:33 +0800

    1、完成player的png透明叠加 2、完成player的png动画播放 3、完成player的方向键控制

--- main.cpp
+++ main.cpp
@@ -1,6 +1,47 @@
-#include <iostream>
 #include <graphics.h>
-#include <conio.h>
+#include <string>
+#include <iostream>
+
+int idx_current_anim = 0; //用于记录当前动画的索引
+
+const int PLAYER_ANIM_NUM = 6; //玩家动画的数量
+
+const int PLAYER_SPEED = 5; //player移动速度
+
+bool is_move_up = false;
+bool is_move_down = false;
+bool is_move_left = false;
+bool is_move_right = false;
+
+IMAGE img_player_left[PLAYER_ANIM_NUM];
+IMAGE img_player_right[PLAYER_ANIM_NUM];
+
+POINT player_pos = { 500,500 }; //玩家初始位置
+
+//实现png透明通道混叠
+#pragma comment(lib,"MSIMG32.LIB")
+
+inline void putimage_alpha(int x, int y, IMAGE* img)
+{
+    int w = img->getwidth();
+    int h = img->getheight();
+    AlphaBlend(GetImageHDC(NULL), x, y, w, h,
+               GetImageHDC(img), 0, 0, w, h, { AC_SRC_OVER,0,255,AC_SRC_ALPHA });
+}
+
+void LoadAnimation()
+{
+    for(size_t i=0;i<PLAYER_ANIM_NUM;i++)
+    {
+        std::string path = "../img/player_left_"+std::to_string(i)+".png";
+        loadimage(&img_player_left[i], path.c_str());
+    }
+    for(size_t i=0;i<PLAYER_ANIM_NUM;i++)
+    {
+        std::string path = "../img/player_right_"+std::to_string(i)+".png";
+        loadimage(&img_player_right[i],(const char*)path.c_str());
+    }
+}
 
 int main() {
     initgraph(1280,720);
@@ -10,6 +51,7 @@ int main() {
     ExMessage msg;
     IMAGE img_background;
 
+    LoadAnimation();
     loadimage(&img_background,_T("../img/background.png"));
 
     BeginBatchDraw();
@@ -20,12 +62,64 @@ int main() {
 
         while(peekmessage(&msg))
         {
+            if (msg.message == WM_KEYDOWN)
+            {
+                switch (msg.vkcode)
+                {
+                    case VK_UP:
+                        is_move_up = true;
+                        break;
+                    case VK_DOWN:
+                        is_move_down = true;
+                        break;
+                    case VK_LEFT:
+                        is_move_left = true;
+                        break;
+                    case VK_RIGHT:
+                        is_move_right = true;
+                        break;
+                }
+            }
+            else if (msg.message == WM_KEYUP)
+            {
+                switch (msg.vkcode)
+                {
+                    case VK_UP:
+                        is_move_up = false;
+                        break;
+                    case VK_DOWN:
+                        is_move_down = false;
+                        break;
+                    case VK_LEFT:
+                        is_move_left = false;
+                        break;
+                    case VK_RIGHT:
+                        is_move_right = false;
+                        break;
+                }
+            }
 
+            if (is_move_up)
+                player_pos.y -= PLAYER_SPEED;
+            if (is_move_down)
+                player_pos.y += PLAYER_SPEED;
+            if (is_move_left)
+                player_pos.x -= PLAYER_SPEED;
+            if (is_move_right)
+                player_pos.x += PLAYER_SPEED;
         }
+
+        static int counter = 0; //用于记录动画帧一共播放了几个游戏帧
+        if(++counter % 5 == 0)
+            idx_current_anim++; //每5帧切换一次动画
+
+        idx_current_anim = idx_current_anim % PLAYER_ANIM_NUM; //确保索引在0~5之间
+
 
         cleardevice();
 
         putimage(0,0,&img_background);
+        putimage_alpha(player_pos.x,player_pos.y,&img_player_right[idx_current_anim]);
 
         FlushBatchDraw();
 
